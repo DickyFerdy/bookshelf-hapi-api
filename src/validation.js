c22b540ce9e343eb16d69554e336f614648d701a
unknown <dickyferdy5@gmail.com>
2023-11-13 17:45:56 +0700

    change max year

--- src/validation.js
+++ src/validation.js
@@ -1,11 +1,11 @@
+/* eslint-disable newline-per-chained-call */
 const Joi = require('joi');
 
 const validator = (schema) => (payload) => schema.validate(payload);
 
 const bookSchema = Joi.object().keys({
   name: Joi.string().required(),
-  year: Joi.number().integer().min(1800).max(2023)
-    .required(),
+  year: Joi.number().integer().min(1800).max(new Date().getFullYear()).required(),
   author: Joi.string().required(),
   summary: Joi.string(),
   publisher: Joi.string().required(),
